f255639e8d49259c00149835bc62d9a10f3ee963
Andres Ruiz <andres87aldos@gmail.com>
2021-01-12 08:59:25 -0600

    Clase 5

---
+++ expresiones.php
@@ -0,0 +1,11 @@
+<?php
+// / - Contenedor
+// ^ - Inicio
+// $ - Final
+// - Rangos (0-9)
+// [] - Patron [0-9]
+// {} - Condición
+
+$password = '123456';
+// de 6 a 9 digitos numericos
+var_dump((bool) preg_match('/^$[0-9]{6,9}/',$password));
